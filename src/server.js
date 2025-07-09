fc544f62061096098945a115f1b79203e069fd29
gazzaar <gazzarDev@gmail.com>
2025-07-09 03:59:42 +0300

    configure sessions

--- src/server.js
+++ src/server.js
@@ -1,11 +1,43 @@
 import bodyParser from 'body-parser';
+import { fileURLToPath } from 'url';
+import path from 'path';
 import dotenv from 'dotenv';
 import express from 'express';
 import session from 'express-session';
 import passport from 'passport';
+import { PrismaClient } from './generated/prisma/client.js';
+import { PrismaSessionStore } from '@quixo3/prisma-session-store';
 dotenv.config();
 
 const app = express();
+const prisma = new PrismaClient();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Init Prisma Store
+const prismaStore = new PrismaSessionStore(prisma, {
+  checkPeriod: 2 * 60 * 1000, //ms
+  dbRecordIdIsSessionId: true,
+  dbRecordIdFunction: undefined,
+});
+
+app.use(
+  session({
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24, // 1 Day
+    },
+    secret: 'process.env.SECRET_KEY',
+    resave: false,
+    saveUninitialized: true,
+    store: prismaStore,
+  }),
+);
+
+app.get('/', (req, res) => {
+  res.send('Welcome back bro!');
+  res.end();
+});
 
 const PORT = process.env.PORT || 3000;
 
