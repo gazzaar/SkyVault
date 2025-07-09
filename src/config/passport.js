64981ab2533260b0c0c23cd3793413d449f59534
gazzaar <gazzarDev@gmail.com>
2025-07-09 07:10:05 +0300

    configure passport

---
+++ src/config/passport.js
@@ -0,0 +1,46 @@
+import bcrypt from 'bcryptjs';
+import passport from 'passport';
+import LocalStrategy from 'passport-local';
+import { PrismaClient } from '../generated/prisma/client.js';
+const prisma = new PrismaClient();
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await prisma.user.findUnique({
+        where: { username: username },
+      });
+      if (!user) {
+        return done(null, false, { message: 'Invalid email or password' });
+      }
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        return done(null, false, { message: 'Invalid email or password' });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  }),
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const { rows } = await prisma.user.findUnique({
+      where: { id: id },
+    });
+    const user = rows[0];
+    if (!user) {
+      return done(null, false);
+    }
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+export default passport;
