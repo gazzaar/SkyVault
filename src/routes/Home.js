fe84b7e4c966e812b5f904ca97d9936e702ca5c8
gazzaar <gazzarDev@gmail.com>
2025-07-12 08:32:39 +0300

    use getFolder when render home

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,13 +1,16 @@
 import { Router } from 'express';
+import { getFolders } from '../controllers/FoldersController.js';
 const homeRouter = Router();
 
-homeRouter.get('/', (req, res) => {
-  if (!req.isAuthenticated()) {
-    return res.redirect('login');
-  }
-  res.render('home', {
-    currentUser: req.user,
-  });
-});
+// homeRouter.get('/', (req, res) => {
+//   if (!req.isAuthenticated()) {
+//     return res.redirect('login');
+//   }
+//   res.render('home', {
+//     currentUser: req.user,
+//   });
+// });
+//
+homeRouter.get('/', getFolders);
 
 export default homeRouter;
