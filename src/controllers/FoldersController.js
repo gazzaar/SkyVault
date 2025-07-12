116338079f246e5393dd97cf7729939649fbb2df
gazzaar <gazzarDev@gmail.com>
2025-07-12 09:08:58 +0300

    method for creating, updating, getting folders

---
+++ src/controllers/FoldersController.js
@@ -0,0 +1,142 @@
+import { body, validationResult } from 'express-validator';
+import { PrismaClient } from '../generated/prisma/client.js';
+const prisma = new PrismaClient();
+
+const validateFolderName = [
+  body('name').trim().notEmpty().withMessage('Folder name should has a name'),
+];
+export const createFolder = [
+  validateFolderName,
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!req.isAuthenticated()) {
+        return res.redirect('login');
+      }
+      if (!errors.isEmpty()) {
+        return res.render('home', {
+          errors: errors.array(),
+        });
+      }
+      const { name } = req.body;
+
+      const existingFolder = await prisma.folder.findFirst({
+        where: {
+          userID: +req.user.id,
+          name: name,
+        },
+      });
+
+      if (existingFolder) {
+        return res.render('home', {
+          errors: [
+            { msg: 'A folder with this name already exists for this user' },
+          ],
+        });
+      }
+      await prisma.folder.create({
+        data: {
+          name: name,
+          userID: req.user.id,
+        },
+      });
+
+      res.redirect('/home');
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
+  },
+];
+
+export const getFolders = async (req, res) => {
+  try {
+    if (!req.isAuthenticated()) {
+      return res.redirect('login');
+    }
+
+    const folders = await prisma.folder.findMany({
+      where: { userID: req.user.id },
+    });
+    res.render('home', {
+      folders: folders,
+      currrentUser: req.user,
+    });
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+};
+
+export const deleteFolder = async (req, res) => {
+  if (!req.isAuthenticated()) {
+    return res.redirect('login');
+  }
+  try {
+    await prisma.folder.delete({
+      where: {
+        id: +req.params.id,
+      },
+    });
+
+    res.redirect('/home');
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const updateFolderName = [
+  validateFolderName,
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!req.isAuthenticated()) {
+        return res.redirect('login');
+      }
+      if (!errors.isEmpty()) {
+        return res.render('home', {
+          errors: errors.array(),
+        });
+      }
+      const { name } = req.body;
+      const id = +req.params.id;
+      const folder = await prisma.folder.findUnique({
+        where: { id: id },
+      });
+      if (!folder) {
+        return res.render('home', {
+          errors: [{ msg: 'No folder' }],
+        });
+      }
+      const existingFolder = await prisma.folder.findFirst({
+        where: {
+          userID: folder.userID,
+          name: name,
+          id: { not: id },
+        },
+      });
+
+      if (existingFolder) {
+        return res.render('home', {
+          errors: [
+            { msg: 'A folder with this name already exists for this user' },
+          ],
+        });
+      }
+      await prisma.folder.update({
+        where: {
+          userID: req.user.id,
+          id: id,
+        },
+        data: {
+          name: name,
+        },
+      });
+
+      res.redirect('/home');
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
+  },
+];
