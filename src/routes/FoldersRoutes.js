db035c7456e19967ca3db51230bacc7e39a10e93
gazzaar <gazzarDev@gmail.com>
2025-07-12 09:31:18 +0300

    add route for signle folder

--- src/routes/FoldersRoutes.js
+++ src/routes/FoldersRoutes.js
@@ -4,14 +4,15 @@ import {
   createFolder,
   deleteFolder,
   updateFolderName,
+  getFolder,
 } from '../controllers/FoldersController.js';
 const folderRouter = Router();
 folderRouter.use(methodOverride('_method'));
 
-// folderRouter.post('/add',)
-folderRouter.get('/', (req, res) => {});
+// folderRouter.get('/', (req, res) => {});
 folderRouter.post('/new', createFolder);
 folderRouter.delete('/:id', deleteFolder);
 folderRouter.put('/:id', updateFolderName);
+folderRouter.get('/:id', getFolder);
 
 export default folderRouter;
